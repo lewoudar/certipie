9448c95ffb443df63a1e16242ecd344ffc302337
le_woudar <rollandkev@yahoo.fr>
2022-03-06 17:11:33 +0100

    feat(cli): updated `cert` command
    Now `-h` option can be used to print help text

--- certipie/cli/main.py
+++ certipie/cli/main.py
@@ -9,9 +9,11 @@ from .commands.server import server
 
 
 @click.version_option('0.1.0', message='%(prog)s version %(version)s')
-@click.group(cls=DYMGroup)
+@click.group(cls=DYMGroup, context_settings={'help_option_names': ['-h', '--help']})
 def cert():
-    """A cli to generate certificate csr and self-signed certificate that can be used for testing purpose."""
+    """
+    A cli to generate certificate signing request and self-signed certificate that can be used for testing purpose.
+    """
 
 
 for command in [rsa, csr, auto_certificate, server, install_completion]:
