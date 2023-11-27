5a7fe63fba9d7dd764eb53700b91d598c6a2b084
le_woudar <rollandkev@yahoo.fr>
2023-11-27 10:09:19 +0100

    refactor(core): used validators library to validate a domain name

--- certipie/core.py
+++ certipie/core.py
@@ -1,11 +1,11 @@
 import ipaddress
-import re
 import uuid
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from typing import AnyStr, Optional, Protocol, Union, runtime_checkable
 
 import idna
+import validators
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -57,15 +57,6 @@ def get_public_key_from_private_key(file_path: Path, private_key: rsa.RSAPrivate
         f.write(private_key.public_key().public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo))
 
 
-# shamefully copied from the validators library
-domain_pattern = re.compile(
-    r'^(?:[a-zA-Z0-9]'  # First character of the domain
-    r'(?:[a-zA-Z0-9-_]{0,61}[A-Za-z0-9])?\.)'  # Sub domain + hostname
-    r'+[A-Za-z0-9][A-Za-z0-9-_]{0,61}'  # First 61 characters of the gTLD
-    r'[A-Za-z]$'  # Last character of the gTLD
-)
-
-
 def get_idn_domain_name(domain_name: str) -> str:
     if domain_name.startswith('*.'):
         idn = b'*.' + idna.encode(domain_name[2:], uts46=True)
@@ -76,7 +67,7 @@ def get_idn_domain_name(domain_name: str) -> str:
 def is_domain_name(domain_name: str) -> bool:
     domain_name = domain_name[2:] if domain_name.startswith('*.') else domain_name
     try:
-        return domain_pattern.match(get_idn_domain_name(domain_name)) is not None
+        return validators.domain(get_idn_domain_name(domain_name)) is True
     except idna.IDNAError:
         return False
 
